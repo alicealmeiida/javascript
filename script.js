c8a1c0249116efccc7afb0968694572d8e9f4680
alicealmeiida <115549489+alicealmeiida@users.noreply.github.com>
2022-12-13 19:35:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let name= "Alice";
+console.log (name);
+let ageE= 18;
+console.log (ageE);
+
+let age7 = ageM*7;
+console.log (age7);
+
+let friendsL = ["Inês", "Diogo", "Yuri"];
+let numbers = [1, 7, 8];
+console.log(friends[0]);
+
+let friends=[
+    {
+        name: "Inês",
+        age: 18,
+        isStudent: true
+    } ,
+    {
+        name: "Diogo",
+        age: 18,
+        isStudent: true
+    },
+    {
+        name: "Yuri",
+        age: 17,
+        isStudent: true
+    }
+
+]
+
+console.log(friends.name);
